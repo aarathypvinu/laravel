aaa0c3217180b9accc4c0c5e95719b92c5d670c5
aarathypvinu <aarathypvinu@gamil.com>
2021-05-26 20:59:00 +0530

    qwel

--- database/migrations/2021_05_18_125421_create_chairs_table.php
+++ database/migrations/2021_05_18_125421_create_chairs_table.php
@@ -14,6 +14,7 @@ class CreateChairsTable extends Migration
     public function up()
     {
         Schema::create('chairs', function (Blueprint $table) {
+            $table->id();
             $table->string("Model");
             $table->string("Description");
             $table->string("Material");
